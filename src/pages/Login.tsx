ebd6d95d03886e62df22922cc689e56c12af2a3d
오수완 <ohsuwan1@gmail.com>
2021-03-12 01:23:02 +0900

    test1

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { axiosLogin } from 'api/user';
+
+interface PropsTyped {
+  email: string;
+  password: string;
+}
 
 const Login = () => {
-  return <div>Login</div>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <>
+      <form action="">
+        <label htmlFor="id">아이디</label>
+        <input value={email} name="id" type="text" />
+        <label htmlFor="password">비밀번호</label>
+        <input value={password} name="password" type="password" />
+      </form>
+      <button onClick={axiosLogin}>Login</button>
+    </>
+  );
 };
 
 export default Login;
